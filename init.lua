3bda104638fbd30b3ad7c2c824039b84863d0a31
Noah <noah.marquie@gmail.com>
2026-06-29 11:06:01 -0700

    Additional changes to lsp for convenience

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.keymap.set('n', '<leader>t', ':botright 15split | terminal<CR>i', { silent = true })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,6 +2,9 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      servers = {
+        pyright = { enabled = false };
+      },
       inlay_hints = { enabled = false },
     },
   },
